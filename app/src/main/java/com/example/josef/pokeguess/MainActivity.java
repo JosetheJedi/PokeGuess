2a284f5f0147a1f5241f4a5c2dea0996b9b4b692
Jose Hernandez <josefhu15@gmail.com>
2017-09-13 02:58:52 -0700

    fixed mainactivity

--- app/src/main/java/com/example/josef/pokeguess/MainActivity.java
+++ app/src/main/java/com/example/josef/pokeguess/MainActivity.java
@@ -1,18 +1,40 @@
 package com.example.josef.pokeguess;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button battleB, shadowB, typeB, pokedexB, leaderboardB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         getSupportActionBar().setTitle("");
+
+        battleB = (Button) findViewById(R.id.battle_cry);
+        shadowB = (Button) findViewById(R.id.shadow_button);
+        typeB = (Button) findViewById(R.id.type_button);
+        pokedexB = (Button) findViewById(R.id.pokedex_button);
+        leaderboardB = (Button) findViewById(R.id.leaderboards_button);
+
+        battleB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BattleCryQuiz.class);
+                startActivity(intent);
+            }
+        });
     }
 }
