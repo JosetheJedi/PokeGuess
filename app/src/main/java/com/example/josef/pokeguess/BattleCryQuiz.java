e24ba379a7fb1ad09192e35a5c5aebee98599a88
Jose Hernandez <josefhu15@gmail.com>
2017-09-21 20:26:47 -0700

    Added battle music to Battlecryquiz

--- app/src/main/java/com/example/josef/pokeguess/BattleCryQuiz.java
+++ app/src/main/java/com/example/josef/pokeguess/BattleCryQuiz.java
@@ -2,12 +2,14 @@ package com.example.josef.pokeguess;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 public class BattleCryQuiz extends AppCompatActivity {
 
+    MediaPlayer media;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,21 @@ public class BattleCryQuiz extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         getSupportActionBar().setTitle("");
+
+        media = MediaPlayer.create(this, R.raw.pokemon_battle);
+        media.setLooping(true);
+        media.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        media.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        media.start();
     }
 }
